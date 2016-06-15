0e808e27734930c16784707bb6c28bf031f65952
petrero <petrero@gmail.com>
2016-06-15 21:25:59 +0300

    External page submission.

---
+++ form_external.php
@@ -0,0 +1,18 @@
+<?php
+  
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <form action="form_process.php" method="POST">
+      <input type="text" name="username" placeholder="Enter Username"/><br>
+      <input type="password" name="password" placeholder="Enter Password"/><br>
+      <input type="submit" name="submit"/>
+    </form>
+  </body>
+</html>
