5559dee501283a8370d4c857025c36521c240d99
petrero <petrero@gmail.com>
2016-06-15 19:01:55 +0300

    Variable written outside the function is only available outside the function. The attempt to change that variable inside the function failed.

---
+++ scope.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <?php 
+      $x = "outside"; //global
+      function convert(){
+        $x = "inside"; //local
+      }
+      
+      echo $x . "<br>";
+      
+      convert();
+      echo $x;
+    ?>
+  </body>
+</html>
