ea85402978845f19e4765a6b18ca00c829d98ecf
petrero <petrero@gmail.com>
2016-06-15 18:56:55 +0300

    Some functions using parameters.

---
+++ passing_params_functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <?php 
+      /*
+      function greeting($message){
+        echo $message;
+      }
+      greeting("Hello Student");
+      */
+    
+      function calculate($number1, $number2){
+        $sum = $number1 + $number2;
+        echo $sum;
+      }
+      
+      calculate(12, 24);
+    ?>
+  </body>
+</html>
