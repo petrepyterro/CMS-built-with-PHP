657e94567722a7eb2b735b9c2985ac3229fb49ec
petrero <petrero@gmail.com>
2016-06-15 20:09:33 +0300

    Validating the form values. Check if the value introduced in name input is in an array variable. If it is echo an welcome message if not the user is not allowed to login to our site. We check the length of the name value either.

--- form.php
+++ form.php
@@ -1,10 +1,24 @@
 <?php
   if (isset($_POST['submit'])){
+    $name = array("Edwin", "Student","Peter", "Samid", "Mohad", "Maria", "Jane", "Tom");
+    $minimum = 5;
+    $maximum = 10;
     $username = $_POST['username'];
     $password = $_POST['password'];
     
-    echo "Hello " . $username;
-    echo "Your password is " . $password;
+    if (strlen($username) < $minimum){
+      echo "Username has to be longer than $minimum";
+    }
+    
+    if (strlen($username) > $maximum){
+      echo "Username cannot be longer than $maximum";
+    }
+    
+    if(!in_array($username, $name)){
+      echo "Sorry you are not allowed to login";
+    } else {
+      echo "Welcome"; 
+    }
   }
 ?>
 <!DOCTYPE html>
