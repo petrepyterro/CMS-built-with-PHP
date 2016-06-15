2d0dd62471080044b249e5697ed763b465fe7be1
petrero <petrero@gmail.com>
2016-06-15 19:07:06 +0300

    Define a constant. We can't change the value of a constant no matter we do. But as we can see that's not the case for a variable.

---
+++ constants.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <?php 
+      $number = 10;
+      $number = 1000;
+      
+      $number = "Edwin";
+      echo $number . "<br>";
+      
+      define("NAME", 1000);
+      echo NAME;
+    ?>
+  </body>
+</html>
