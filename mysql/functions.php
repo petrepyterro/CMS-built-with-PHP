74444e6b50cc37b4904734adc777db5d8f5a49c7
petrero <petrero@gmail.com>
2016-06-16 19:58:00 +0300

    Delete records from database with PHP.

--- mysql/functions.php
+++ mysql/functions.php
@@ -32,3 +32,20 @@ function UpdateTable(){
     die("Query failed" . mysqli_error($connection));
   }
 }
+
+
+function deleteRows(){
+  global $connection;
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $id = $_POST['id'];
+
+  $query = "DELETE FROM users ";
+  $query .= "WHERE id = $id"; 
+
+  $result = mysqli_query($connection, $query);
+
+  if (!$result){
+    die("Query failed" . mysqli_error($connection));
+  }
+}
---
+++ mysql/login_delete.php
@@ -0,0 +1,43 @@
+<?php 
+  include "db.php";
+  include "functions.php";
+  
+  if (isset($_POST['submit'])){
+    deleteRows();
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="col-sm-6">
+        <form action="login_delete.php" method="POST">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" />
+          </div>
+          
+          <div class="form-group">
+            <select name="id" id="">
+              <?php 
+                showAllData();
+              ?>
+            </select>
+          </div>
+          <input type="submit" class="btn btn-primary" name="submit" value="Delete"/>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
