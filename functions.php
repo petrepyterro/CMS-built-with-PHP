4e32e1dd4ad2d4f26415bf72d786d9846408ade2
petrero <petrero@gmail.com>
2016-06-16 21:57:09 +0300

    Password encryption.

--- functions.php
+++ functions.php
@@ -10,6 +10,12 @@ function createRows(){
     $username = mysqli_real_escape_string($connection, $username);
     $password = mysqli_real_escape_string($connection, $password);
     
+    $hashFormat = "$2y$10$";
+    $salt = "iusesomecrazystrings22";
+    $hash_and_salt = $hashFormat . $salt;
+    
+    $password = crypt($password, $hash_and_salt);
+    
     $query = "INSERT INTO users(username, password) ";
     $query .= "VALUES ('$username', '$password')";
     
