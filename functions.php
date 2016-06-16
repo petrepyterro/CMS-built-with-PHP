aa0362bbf1648246c3f3f78fd65ee7bf4347c499
petrero <petrero@gmail.com>
2016-06-16 21:38:26 +0300

    Prevent SQL injection.

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ function createRows(){
     $username = $_POST['username'];
     $password = $_POST['password'];
     
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
     
     $query = "INSERT INTO users(username, password) ";
     $query .= "VALUES ('$username', '$password')";
