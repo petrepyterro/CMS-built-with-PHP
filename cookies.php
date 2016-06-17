f1c9c771b0efc3022db8a05e352f20f3d7e70b58
petrero <petrero@gmail.com>
2016-06-17 21:15:29 +0300

    Reading cookies in PHP.

--- cookies.php
+++ cookies.php
@@ -13,6 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    
+    <?php 
+      if (isset($_COOKIE['someName'])){
+        $someOne = $_COOKIE['someName'];
+        echo $someOne;
+      } else {
+        $someOne = "";
+      }
+    ?>
   </body>
 </html>
