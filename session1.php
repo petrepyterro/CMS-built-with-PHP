97c31734344713f5b2f7c8c369067d281451bf80
petrero <petrero@gmail.com>
2016-06-17 21:29:38 +0300

    Use sessions in PHP.

---
+++ session1.php
@@ -0,0 +1,15 @@
+<?php 
+  session_start();
+  $_SESSION['greeting'] = "Hello Student";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    
+  </body>
+</html>
