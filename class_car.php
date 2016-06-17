5c3ed2afb62df554d9b2524ca496dd9fec17d8db
petrero <petrero@gmail.com>
2016-06-17 22:02:17 +0300

    Instantiate a class a use the function inside that class.

--- class_car.php
+++ class_car.php
@@ -3,11 +3,17 @@
 class Car {
 
     function MoveWheels(){
-      
+      echo "Wheels Move";
     }
 }
 
+/*
 if (method_exists("Car", "MoveWheels")){
   echo "Ya";
 }
+ */
 
+$bmw = new Car();
+$mercedes_benz = new Car();
+
+$bmw->MoveWheels();
