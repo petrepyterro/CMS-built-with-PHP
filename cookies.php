ed2c250e04fb10501260ac929474409e18f007b5
petrero <petrero@gmail.com>
2016-06-17 20:25:01 +0300

    Setting cookies with PHP

---
+++ cookies.php
@@ -0,0 +1,18 @@
+<?php 
+  $name = "someName";
+  $value = 100;
+  $expiration = time() + (60*60*24*7);
+  
+  setcookie($name, $value, $expiration);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    
+  </body>
+</html>
