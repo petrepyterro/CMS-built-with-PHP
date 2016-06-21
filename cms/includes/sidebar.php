40c64009cf21c71977ffe28b1270d21b4c0c7ef1
petrero <petrero@gmail.com>
2016-06-21 22:46:30 +0300

    Check if query created for searching in the database is working properly.

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -1,17 +1,34 @@
 <div class="col-md-4">
-
+  <?php
+    if (isset($_POST['submit'])){
+      $search = $_POST['search'];
+      $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+      $search_query = mysqli_query($connectiuon, $query);
+      if (!$search_query){
+        die("Query failed. " . mysqli_error($connectiuon));
+      }
+      
+      $count = mysqli_num_rows($search_query);
+      if ($count == 0){
+        echo "<h1>NO RESULT</h1>";
+      } else {
+        echo "<h1>SOME RESULT</h1>";
+      }
+    }
+  ?>
   <!-- Blog Search Well -->
   <div class="well">
     <h4>Blog Search</h4>
-    <div class="input-group">
-      <input type="text" class="form-control">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">
-          <span class="glyphicon glyphicon-search"></span>
-        </button>
-      </span>
-    </div>
-    <!-- /.input-group -->
+    <form action="" method="POST">
+      <div class="input-group">
+        <input name="search" type="text" class="form-control">
+        <span class="input-group-btn">
+          <button name="submit" class="btn btn-default" type="submit">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div><!-- /.input-group -->
+    </form>
   </div>
 
   <!-- Blog Categories Well -->
