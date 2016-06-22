98bf197b415aec0c9557e48044b50ccc5c2e56ed
petrero <petrero@gmail.com>
2016-06-22 16:56:12 +0300

    Copy already created code from branch 12.

---
+++ cms/includes/sidebar.php
@@ -0,0 +1,44 @@
+<div class="col-md-4">
+  <!-- Blog Search Well -->
+  <div class="well">
+    <h4>Blog Search</h4>
+    <form action="search.php" method="POST">
+      <div class="input-group">
+        <input name="search" type="text" class="form-control">
+        <span class="input-group-btn">
+          <button name="submit" class="btn btn-default" type="submit">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div><!-- /.input-group -->
+    </form>
+  </div>
+
+  <!-- Blog Categories Well -->
+  <div class="well">
+    <?php 
+      $query = "SELECT * FROM categories";
+      $select_categories_sidebar = mysqli_query($connectiuon, $query);
+
+      
+    ?>
+    <h4>Blog Categories</h4>
+    <div class="row">
+      <div class="col-lg-12">
+        <ul class="list-unstyled">
+          <?php 
+            while($row = mysqli_fetch_assoc($select_categories_sidebar)){
+              $cat_title = $row['cat_title'];
+              echo "<li><a href='#'>$cat_title</a></li>";
+            }
+          ?>
+        </ul>
+      </div><!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+  </div>
+
+  <!-- Side Widget Well -->
+  <?php include "includes/widget.php"; ?>
+
+</div>
