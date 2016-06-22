18e070534e6ca8060f0ca3bf1381ecd27ba14b0d
petrero <petrero@gmail.com>
2016-06-22 22:10:58 +0300

    Update categories.

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -40,28 +40,12 @@
                 <input type="submit" class="btn btn-primary" name="submit" value="Add Category" />
               </div>
             </form>
-            <form action="" method="POST">
-              <div class="form-group">
-                <label for="cat-title">Edit Category</label>
-                <?php
-                  if (isset($_GET['edit'])){
-                    $cat_id = $_GET['edit'];
-                    $query = "SELECT * FROM categories WHERE cat_id=$cat_id ";
-                    $select_categories_id = mysqli_query($connection, $query);
-                    while($row = mysqli_fetch_assoc($select_categories_id)){
-                      $cat_id = $row['cat_id'];
-                      $cat_title = $row['cat_title'];
-                     ?>
-                     <input type="text" value="<?php if (isset($cat_title))echo $cat_title ?>" class="form-control" name="cat_title" />
-                  <?php
-                  }}                    
-                ?>
-                
-              </div>
-              <div class="form-group">
-                <input type="submit" class="btn btn-primary" name="submit" value="Update Category" />
-              </div>
-            </form>
+            <?php 
+              if (isset($_GET['edit'])){
+                $cat_id = $_GET['edit'];
+                include "includes/update_categories.php";
+              }
+            ?>
           </div>
           <div class="col-xs-6">
             <table class="table table-bordered table-hover">
