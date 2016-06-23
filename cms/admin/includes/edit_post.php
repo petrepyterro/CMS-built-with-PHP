c7814293ad5b391a0766fa178ada9555842c14f3
petrero <petrero@gmail.com>
2016-06-23 23:55:08 +0300

    Finally updating posts. Because inputs of type files don't send to  superglobal any value when you don't choose a file to upload you must get sure that variable post_image is always populated either by value send by input or from database for the image above the input file always function.

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -15,7 +15,42 @@
       $post_tags = $row['post_tags'];
       $post_comment_count = $row['post_comment_count'];
       $post_date = $row['post_date'];
-    }  
+    }
+    
+  }
+  if(isset($_POST['update_post'])){
+    $post_title = $_POST['post_title'];
+    $post_author = $_POST['post_author'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['post_image']['name'];
+    $post_image_temp = $_FILES['post_image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+    if(strlen(trim($post_image)) == 0){
+      $query = "SELECT * FROM posts WHERE post_id=$the_post_id";
+      $select_image = mysqli_query($connection, $query);
+      
+      while ($row = mysqli_fetch_assoc($select_image)){
+        $post_image = $row['post_image'];
+      }
+    }
+    
+    $query = 'UPDATE posts SET ';
+    $query .= "post_title = '$post_title', "; 
+    $query .= "post_category_id = $post_category_id, "; 
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '$post_author', "; 
+    $query .= "post_status = '$post_status', "; 
+    $query .= "post_tags = '$post_tags', "; 
+    $query .= "post_content = '$post_content', "; 
+    $query .= "post_image = '$post_image' "; 
+    $query .= "WHERE post_id = $the_post_id";
+    
+    $update_post = mysqli_query($connection, $query);
+    confirmQuery($update_post, $query);
   }
 ?>
 <form action="" method="POST" enctype="multipart/form-data">
@@ -24,7 +59,7 @@
     <input type="text" value="<?php echo $post_title ?>" name="post_title" class="form-control" />
   </div>
   <div class="form-group">
-    <select name="" id="">
+    <select name="post_category" id="">
       <?php
         $query = "SELECT * FROM categories";
         $select_categories = mysqli_query($connection, $query);
@@ -47,6 +82,7 @@
   </div>
   <div class="form-group">
     <img width="100" src="../images/<?php echo $post_image?>" alt=""/>
+    <input type="file" name="post_image"/>
   </div>
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
@@ -58,6 +94,6 @@
     </textarea>
   </div>
   <div class="form-group">
-    <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post" />
+    <input type="submit" class="btn btn-primary" name="update_post" value="Update Post" />
   </div>
 </form>
