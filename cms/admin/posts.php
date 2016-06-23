cd55a411c3db43a5749b48064b02583800a26b1e
petrero <petrero@gmail.com>
2016-06-23 17:46:44 +0300

    Displaying posts list in admin.

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -28,17 +28,35 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>10</td>
-                <td>Gigi</td>
-                <td>Bootstrap Framework</td>
-                <td>Bootstrap</td>
-                <td>Status</td>
-                <td>Image</td>
-                <td>Tags</td>
-                <td>Comments</td>
-                <td>Date</td>
-              </tr>
+              <?php 
+                $query = "SELECT * FROM posts";
+                $select_posts = mysqli_query($connection, $query);
+                
+                while ($row = mysqli_fetch_assoc($select_posts)){
+                  $post_id = $row['post_id'];
+                  $post_title = $row['post_title'];
+                  $post_author = $row['post_author'];
+                  $post_category_id = $row['post_category_id'];
+                  $post_status = $row['post_status'];
+                  $post_image = $row['post_image'];
+                  $post_tags = $row['post_tags'];
+                  $post_comment_count = $row['post_comment_count'];
+                  $post_date = $row['post_date'];
+                  
+                  echo "<tr>";
+                  echo "<td>$post_id</td>";
+                  echo "<td>$post_author</td>";
+                  echo "<td>$post_title</td>";
+                  echo "<td>$post_category_id</td>";
+                  echo "<td>$post_status</td>";
+                  echo "<td><img width='100' src='../images/$post_image' /></td>";
+                  echo "<td>$post_tags</td>";
+                  echo "<td>$post_comment_count</td>";
+                  echo "<td>$post_date</td>";
+                  echo "</tr>";
+                }
+              ?>
+              
             </tbody>
           </table>
         </div>
