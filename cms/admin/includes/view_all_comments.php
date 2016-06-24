4f3992f3f23d59ed1b52d0f8401ca499b0105c45
petrero <petrero@gmail.com>
2016-06-24 23:45:01 +0300

    Delete comments from our comments admin page.

--- cms/admin/includes/view_all_comments.php
+++ cms/admin/includes/view_all_comments.php
@@ -53,7 +53,7 @@
         echo "<td>$comment_date</td>";
         echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
         echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
-        echo "<td><a href='posts.php?delete='>Delete</a></td>";
+        echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
         echo "</tr>";
       }
     ?>
@@ -63,10 +63,10 @@
 
 <?php 
   if (isset($_GET['delete'])){
-    $the_post_id = $_GET['delete'];
-    $query = "DELETE FROM posts WHERE post_id=$the_post_id";
+    $the_comment_id = $_GET['delete'];
+    $query = "DELETE FROM comments WHERE comment_id=$the_comment_id";
     $delete_query = mysqli_query($connection, $query);
     
-    header("Location: posts.php");
+    header("Location: comments.php");
   }
 ?>
