06a4f114ba83d06eed9aa37acc2859d87995faf6
petrero <petrero@gmail.com>
2016-06-24 20:19:32 +0300

    Create individual post pages and make alink to them from the homepage.

--- cms/index.php
+++ cms/index.php
@@ -20,6 +20,7 @@
               $select_all_posts_query = mysqli_query($connection, $query);
           
               while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                $post_id = $row['post_id'];
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -35,14 +36,14 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://localhost/images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="http://localhost/images/<?php echo $post_image ?>" alt=""></a>
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
