6069eca29f1f81f8bfbd422ad87eb9b2833999a8
petrero <petrero@gmail.com>
2016-06-25 18:48:37 +0300

    Simplify the workflow of login process in login.php page.

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -21,17 +21,19 @@
       $db_user_role = $row['user_role'];
     }
     
-    if ($username !== $db_username && $password !== $db_user_password){
-      header("Location: ../index.php");
-    } else if($username == $db_username && $password == $db_user_password){
+    if ($username === $db_username && $password === $db_user_password){
       $_SESSION['username'] = $db_username;
       $_SESSION['firstname'] = $db_user_firstname;
       $_SESSION['lastname'] = $db_user_lastname;
       $_SESSION['role'] = $db_user_role;
-      header("Location: ../admin");
       
+      header("Location: ../admin");
     } else {
+      $_SESSION['username'] = $db_username;
+      $_SESSION['firstname'] = $db_user_firstname;
+      $_SESSION['lastname'] = $db_user_lastname;
+      $_SESSION['role'] = $db_user_role;
       header("Location: ../index.php");
-    }
+    } 
   }
 ?>
