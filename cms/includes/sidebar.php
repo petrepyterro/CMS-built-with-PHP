43d8063ccc6ce12fa7d69cfdc6efe7cbbad2bd99
petrero <petrero@gmail.com>
2016-06-25 16:57:40 +0300

    Creating the login form

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -14,6 +14,21 @@
     </form>
   </div>
 
+  <!-- Login -->
+  <div class="well">
+    <h4>Login</h4>
+    <form action="includes/login.php" method="POST">
+      <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Enter Username">
+      </div><!-- /.input-group -->
+      <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="submit" name="login">Submit</button>
+        </span>
+      </div><!-- /.input-group -->
+    </form>
+  </div>
   <!-- Blog Categories Well -->
   <div class="well">
     <?php 
