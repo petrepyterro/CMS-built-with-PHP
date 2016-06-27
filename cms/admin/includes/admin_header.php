0463f067974e766d98f666736564083b16c70cf7
petrero <petrero@gmail.com>
2016-06-27 22:42:35 +0300

    adding the WYSIWYG editor tinyMCE.

--- cms/admin/includes/admin_header.php
+++ cms/admin/includes/admin_header.php
@@ -39,6 +39,8 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src='http://cdn.tinymce.com/4/tinymce.min.js'></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
   </head>
 
   <body>
