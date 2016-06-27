3b074eb1aae9839547bc65b957ca9e7098c16f2a
petrero <petrero@gmail.com>
2016-06-27 20:39:00 +0300

    Dynamic category selection for edit post page.

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -77,9 +77,15 @@
     <input type="text" value="<?php echo $post_author ?>" class="form-control" name="post_author"/>
   </div>
   <div class="form-group">
-    <label for="post_status">Post Status</label>
-    <input type="text" value="<?php echo $post_status ?>" class="form-control" name="post_status"/>
-  </div>
+    <select name="post_status" id="">
+      <option value="<?php echo $post_status ?>"><?php echo $post_status ?></option>
+      <?php if ($post_status =='published'){ ?>
+        <option value="draft">Draft</option>
+      <?php } else { ?>
+        <option value="published">Published</option>
+      <?php } ?>  
+    </select>
+  </div>  
   <div class="form-group">
     <img width="100" src="../images/<?php echo $post_image?>" alt=""/>
     <input type="file" name="post_image"/>
