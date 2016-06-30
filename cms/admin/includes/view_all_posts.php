77ec2fb23fe4ca94beb734ff807e3ad447c2639a
petrero <petrero@gmail.com>
2016-06-30 22:57:37 +0300

    Reset the tracking viewing posts. Superglobal variable GET is escaped when is inserted in SQL syntax.

--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -44,6 +44,25 @@
             die("Query failed. " . mysqli_error($connection));
           }
           break;
+        case 'reset_views_count':
+          $query = "SELECT post_views_count FROM posts WHERE post_id=$postValueId";
+          $select_post_views_count  = mysqli_query($connection, $query);
+          
+          if (!$select_post_views_count){
+            die("Query failed. " . mysqli_error($connection));
+          }
+          if ($row = mysqli_fetch_array($select_post_views_count)){
+            $views_count = $row['post_views_count'];
+            if ($views_count !== 0){
+              $query = "UPDATE posts SET post_views_count = 0 WHERE post_id=$postValueId";
+              $reset_to_0 = mysqli_query($connection, $query);
+              if (!$reset_to_0){
+                die("Query failed. " . mysqli_error($connection));
+              }
+            }
+            
+          };
+          
       }
     }
   }
@@ -56,7 +75,8 @@
         <option value="published">Publish</option>
         <option value="draft">Draft</option>
         <option value="delete">Delete</option>
-        <option value="clone">Clone</option>
+        <option value="reset_views_count">Reset Views Count</option>
+        
       </select>
     </div>
     <div class="col-xs-4">
@@ -118,7 +138,11 @@
           echo "<td>$post_tags</td>";
           echo "<td>$post_comment_count</td>";
           echo "<td>$post_date</td>";
-          echo "<td>$post_views_count</td>";
+          if ($post_views_count != 0){
+            echo "<td><a href='posts.php?reset=$post_id'>$post_views_count</a></td>";
+          } else {
+            echo "<td>$post_views_count</td>";
+          }
           echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
           echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
           echo "<td><a onclick=\" javascript: return confirm('Are you sure you want to delete');\" href='posts.php?delete=$post_id'>Delete</a></td>";
@@ -134,6 +158,14 @@
   if (isset($_GET['delete'])){
     $the_post_id = $_GET['delete'];
     $query = "DELETE FROM posts WHERE post_id=$the_post_id";
+    $delete_query = mysqli_query($connection, $query);
+    
+    header("Location: posts.php");
+  }
+  
+  if (isset($_GET['reset'])){
+    $the_post_id = $_GET['reset'];
+    $query = "UPDATE posts SET post_views_count = 0 WHERE post_id=" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
     $delete_query = mysqli_query($connection, $query);
     
     header("Location: posts.php");
