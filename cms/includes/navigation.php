ed8ca57a8f38868c5409c63613d7ca55d4a2f515
petrero <petrero@gmail.com>
2016-08-30 18:21:51 +0300

    Send emails.

--- cms/includes/navigation.php
+++ cms/includes/navigation.php
@@ -29,6 +29,10 @@
         <li>
           <a href="registration.php">Registration</a>
         </li>
+        
+        <li>
+          <a href="contact.php">Contact</a>
+        </li>
         <?php
           if (isset($_SESSION['username'])){ 
             if (isset($_GET['p_id'])){
