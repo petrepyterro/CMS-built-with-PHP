79915af06761c4d4ba355388a1aced154aa20b4a
petrero <petrero@gmail.com>
2016-08-30 19:24:23 +0300

    Display a bootstrap confirmation modal dialog box before deleting any post.

--- cms/admin/includes/admin_footer.php
+++ cms/admin/includes/admin_footer.php
@@ -1,8 +1,7 @@
     </div>
     <!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    
     <script src="js/scripts.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -1,4 +1,5 @@
 <?php 
+  include("delete_modal.php");
   if (isset($_POST['checkBoxArray'])){
     foreach($_POST['checkBoxArray'] as $postValueId){
       $bulkOptions = escape($_POST['bulk_options']);
@@ -166,7 +167,8 @@
           }
           echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
           echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
-          echo "<td><a onclick=\" javascript: return confirm('Are you sure you want to delete');\" href='posts.php?delete=$post_id'>Delete</a></td>";
+          echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
+          //echo "<td><a onclick=\" javascript: return confirm('Are you sure you want to delete');\" href='posts.php?delete=$post_id'>Delete</a></td>";
           echo "</tr>";
         }
       ?>
@@ -193,3 +195,16 @@
   }
 ?>
 
+<script type="text/javascript">
+  $(document).ready(function(){
+    $(".delete_link").on('click', function(){
+      var id = $(this).attr("rel");
+      var delete_url = "posts.php?delete=" + id + " ";
+      
+      $(".modal_delete_link").attr("href", delete_url);
+      
+      $('#myModal').modal('show');
+      
+    });
+  });
+</script>
