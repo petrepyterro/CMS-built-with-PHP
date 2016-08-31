fb37d324cf75073ce636c1bc7a580d69e94b26be
petrero <petrero@gmail.com>
2016-08-31 21:34:07 +0300

    Refactoring our view all posts by joining tables.

--- cms/admin/includes/view_all_posts.php
+++ cms/admin/includes/view_all_posts.php
@@ -107,7 +107,11 @@
     </thead>
     <tbody>
       <?php 
-        $query = "SELECT * FROM posts ORDER BY post_id DESC";
+        //$query = "SELECT * FROM posts ORDER BY post_id DESC";
+        $query = "SELECT posts.post_id, posts.post_author, posts.post_user, posts.post_title, posts.post_category_id, posts.post_status, posts.post_image, ";
+        $query .= "posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, categories.cat_id, categories.cat_title";
+        $query .= " FROM posts ";
+        $query .= "LEFT JOIN categories ON posts.post_category_id = categories.cat_id ORDER BY post_id DESC";
         $select_posts = mysqli_query($connection, $query);
 
         while ($row = mysqli_fetch_assoc($select_posts)){
@@ -122,6 +126,8 @@
           $post_comment_count = $row['post_comment_count'];
           $post_date = $row['post_date'];
           $post_views_count = $row['post_views_count'];
+          $category_title = $row['cat_title'];
+          $category_id = $row['cat_id'];
 
           echo "<tr>";
           ?>
@@ -140,13 +146,13 @@
           
           
           echo "<td>$post_title</td>";
-          $query = "SELECT * FROM categories WHERE cat_id=$post_category_id ";
-          $select_categories_id = mysqli_query($connection, $query);
-          while($row = mysqli_fetch_assoc($select_categories_id)){
-            $cat_id = $row['cat_id'];
-            $cat_title = $row['cat_title'];
-            echo "<td>$cat_title</td>";
-          }
+          //$query = "SELECT * FROM categories WHERE cat_id=$post_category_id ";
+          //$select_categories_id = mysqli_query($connection, $query);
+          //while($row = mysqli_fetch_assoc($select_categories_id)){
+          //  $cat_id = $row['cat_id'];
+          //  $cat_title = $row['cat_title'];
+            echo "<td>$category_title</td>";
+          //}
           echo "<td>$post_status</td>";
           echo "<td><img width='100' src='../images/$post_image' /></td>";
           echo "<td>$post_tags</td>";
