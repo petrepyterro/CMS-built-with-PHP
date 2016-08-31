7d88d9bdb954e89496e8533847bd582572412f0f
petrero <petrero@gmail.com>
2016-08-31 19:00:11 +0300

    Make active the item list in the navigation menu that bear the name of our current page.

--- cms/includes/navigation.php
+++ cms/includes/navigation.php
@@ -18,15 +18,29 @@
           $select_all_category_query = mysqli_query($connection, $query);
           
           while($row = mysqli_fetch_assoc($select_all_category_query)){
+            $cat_id = $row['cat_id'];
             $cat_title = $row['cat_title'];
-            echo "<li><a href='#'>$cat_title</a></li>";
+            
+            $category_class = '';
+            $registration_class = '';
+            
+            $pageName = basename($_SERVER['PHP_SELF']);
+            $registration = 'registration.php';
+            
+            if(isset($_GET['category']) && $_GET['category'] == $cat_id){
+              $category_class = 'active';
+            }  elseif($pageName == $registration) {
+              $registration_class = 'active';
+            }
+            
+            echo "<li class=$category_class><a href='category.php?category={$cat_id}'>$cat_title</a></li>";
           }
         ?>
         <li>
           <a href="admin">Admin</a>
         </li>
         
-        <li>
+        <li class="<?php echo $registration_class ?>">
           <a href="registration.php">Registration</a>
         </li>
         
