657fe8e52fff6f25869988fda0629459ad31e58b
petrero <petrero@gmail.com>
2016-08-31 19:41:43 +0300

    Solve bug in edit post page. When editing a post it was passed in bootstrap category before.

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -70,7 +70,12 @@
         while($row = mysqli_fetch_assoc($select_categories)){
           $cat_id = $row['cat_id'];
           $cat_title = $row['cat_title'];
-          echo "<option value='$cat_id'>$cat_title</option>";
+          
+          if($cat_id == $post_category_id){
+            echo "<option selected value='$cat_id'>$cat_title</option>";
+          } else {
+            echo "<option value='$cat_id'>$cat_title</option>";
+          }
         };
       ?>
     </select>
